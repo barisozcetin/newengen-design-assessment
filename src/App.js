c347d9126db39a907cdbff4921547dbd32bf324b
baris_000 <barisozcetin@gmail.com>
2018-09-16 18:51:11 -0700

    Loading spinner added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 import Header from "./components/Header";
+import Loading from "./components/Loading";
 
 class App extends Component {
   state = {
@@ -17,7 +18,9 @@ class App extends Component {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    // const mainColors = Object.keys(this.props.colors.allColors);
+    if (this.props.loading) {
+      return <Loading />;
+    }
     return (
       <div className="app">
         <Header toggleSidebar={this.toggleSidebar} />
@@ -29,7 +32,6 @@ class App extends Component {
             max-height: 100vh;
             overflow: hidden;
             display: grid;
-            // 1 to 4.5 comes from the design
             grid-template-columns: 1fr;
             grid-template-rows: 100px minmax(80vh, 90vh);
             grid-template-areas:
@@ -37,11 +39,9 @@ class App extends Component {
               "content";
           }
 
-          // .content {
-          //   grid-area: content;
-          // }
           @media screen and (min-width: 769px) {
             .app {
+              // 1 to 4.5 comes from the design
               grid-template-columns: 2fr 9fr;
               grid-template-rows: 100px 1fr;
               grid-template-areas:
