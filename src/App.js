d55c17011814149aed57e363269b4686e7a5e95b
baris_000 <barisozcetin@gmail.com>
2018-09-16 17:04:04 -0700

    old code deleted

--- src/App.js
+++ src/App.js
@@ -7,54 +7,56 @@ import SideBar from "./components/SideBar";
 import Header from "./components/Header";
 
 class App extends Component {
+  state = {
+    sidebar: false
+  };
+  toggleSidebar = () => {
+    this.setState(prevState => ({ sidebar: !prevState.sidebar }));
+  };
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    const mainColors = Object.keys(this.props.colors.allColors);
-    // console.log(this.props.colors.allColors);
+    // const mainColors = Object.keys(this.props.colors.allColors);
     return (
       <div className="app">
-        {/* <header className="header">
-          <div className="header--title">
-            <img src="/logo.png" alt="New Engen" />
-          </div>
-          <form className="search--form">
-            <input
-              type="text"
-              defaultValue="Search"
-              className="search--input"
-            />
-          </form>
-        </header> */}
-        <Header />
-        <SideBar />
+        <Header toggleSidebar={this.toggleSidebar} />
+        <SideBar active={this.state.sidebar} />
         <Main />
         <style jsx>{`
           .app {
+            height: 100vh;
             max-height: 100vh;
+            overflow: hidden;
             display: grid;
             // 1 to 4.5 comes from the design
-            grid-template-columns: 2fr 9fr;
+            grid-template-columns: 1fr;
             grid-template-rows: 100px minmax(80vh, 90vh);
             grid-template-areas:
-              "header header"
-              "sidebar content";
+              "header"
+              "content";
           }
 
           // .content {
           //   grid-area: content;
           // }
+          @media screen and (min-width: 769px) {
+            .app {
+              grid-template-columns: 2fr 9fr;
+              grid-template-rows: 100px 1fr;
+              grid-template-areas:
+                "header header"
+                "sidebar content";
+            }
+          }
         `}</style>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ loading, colors, mainColors }) => ({
-  loading,
-  colors,
-  mainColors
+const mapStateToProps = ({ loading }) => ({
+  loading
 });
 
 export default connect(mapStateToProps)(App);
